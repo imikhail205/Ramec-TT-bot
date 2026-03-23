c43d37becca329560c6f9c4b2e912fe5ae32d0c1
imikhail205 <imikhail205@gmail.com>
2026-03-23 17:00:13 +0300

    Add files via upload

---
+++ fb-ramec-bot.py
@@ -0,0 +1,8 @@
+﻿import os
+token = os.getenv('MY_TOKEN')
+channel_fb = '@ramec_football'
+from telebot import TeleBot, types
+bot = TeleBot(token=token)
+ques_fb='✅ Кто в этот четверг идёт на футбол? ⚽'
+list_fb = ['🏃‍♂️', '🏃‍♂️🏃‍♂️', '🏃‍♂️🏃‍♂️🏃‍♂️', '🙅‍♂️']
+bot.send_poll(channel_fb, ques_fb, list_fb, False,'regular', True)
---
+++ tt-ramec-bot.py
@@ -0,0 +1,8 @@
+﻿import os
+token = os.getenv('MY_TOKEN')
+channel_tt = '@ramec_tt'
+from telebot import TeleBot, types
+bot = TeleBot(token=token)
+ques_tt='✅ Запись на следующие тренировки 🏓'
+list_tt = ['ПН - 18:00 стол 1-2', 'ЧТ - 18:30 стол 1-2', 'ПТ - 18:00 стол 1-2', 'Следить 🔍']
+bot.send_poll(channel_tt, ques_tt, list_tt, False,'regular', True)
