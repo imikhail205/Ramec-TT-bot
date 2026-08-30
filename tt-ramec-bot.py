3b89e65402cf9a0f7a89b45204a021b1b2508a1e
imikhail205 <imikhail205@gmail.com>
2026-08-30 18:24:00 +0300

    Update tt-ramec-bot.py

--- tt-ramec-bot.py
+++ tt-ramec-bot.py
@@ -4,5 +4,5 @@ channel_tt = '@ramec_tt'
 from telebot import TeleBot, types
 bot = TeleBot(token=token)
 ques_tt='✅ Запись на следующие тренировки 🏓'
-list_tt = ['ПН - 18:00 стол 1-2', СР - 18:00 стол 1-2', 'ЧТ - 18:30 стол 1-2', 'Следить 🔍']
+list_tt = ['ПН - 18:00 стол 1-2', 'СР - 18:00 стол 1-2', 'ЧТ - 18:30 стол 1-2', 'Следить 🔍']
 bot.send_poll(channel_tt, ques_tt, list_tt, False,'regular', True)
